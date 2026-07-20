6ab26964497f929cd1c379209b42d5d1c1136bb7
Ravi Saroj <Ravi.Saroj@coforge.com>
2026-07-20 14:23:19 +0530

    hello

--- obj/Debug/net10.0/product.AssemblyInfo.cs
+++ obj/Debug/net10.0/product.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("product")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90799b02a6f22466c20908221bc401cfad39ef8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("product")]
 [assembly: System.Reflection.AssemblyTitleAttribute("product")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
